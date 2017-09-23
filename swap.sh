6791746de50a548c2ddc7d0b3e1bef0c58ba8307
yosh778 <yosh778@gmail.com>
2017-09-24 01:03:04 +0200

    Added file swapper script + 1stload list

---
+++ swap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+display_usage()
+{
+    echo "Usage : $0 <fileList> <inputDir> <outputDir>" 
+    echo -e "  Copies the filenames in <fileList> from <inputDir> to <outputDir>" 
+}
+
+if [  $# -lt 3 ] 
+then 
+    display_usage
+    exit 1
+fi 
+
+input=$2
+output=$3
+
+while IFS='' read -r filename || [[ -n "$filename" ]]; do
+
+    echo "Copying $filename from $input to $output"
+    cp -f "$input"/"$filename" "$output"/"$filename"
+
+done < "$1"
+
+echo "Script $1 finished"
+
