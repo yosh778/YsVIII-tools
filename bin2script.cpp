c0966de98bfb65d34d9336eb955ec71ee77519a6
yosh778 <yosh778@gmail.com>
2017-10-09 20:01:09 +0200

    New float hex prefix

--- bin2script.cpp
+++ bin2script.cpp
@@ -203,7 +203,7 @@ void process_segment( std::ifstream& fh, SEGMENT_HEADER& segHead )
 
 			case FLOAT_TAG:
 				// std::cout << ", f" << arg.fVal;
-				std::cout << ", fx" << std::hex << arg.uVal << std::dec;
+				std::cout << ", .0x" << std::hex << arg.uVal << std::dec;
 				std::cout << " (~" << arg.fVal << ")";
 				break;
 
